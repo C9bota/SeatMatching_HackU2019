3324f458aba254ce1d71e090f947ffea74fe3d76
9bo <>
2019-07-30 20:09:47 +0900

    first commit

---
+++ SeatMatchingiOS/SeatMatchingiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeatMatchingiOS
+//
+//  Created by yobiduck on 2019/07/30.
+//  Copyright © 2019 yobiduck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
