4925217dd8963c468e89cbe40b48fbee08860b4d
9bo <>
2019-08-05 21:34:42 +0900

    ViewController内で緯度経度をprintで表示（GPSController使ってない）

--- SeatMatchingiOS/SeatMatchingiOS/GPSController.swift
+++ SeatMatchingiOS/SeatMatchingiOS/GPSController.swift
@@ -11,6 +11,33 @@ import CoreLocation
 
     class GPSController{
     
+        var locationManager: CLLocationManager!
+
+        init() {
+            setupLocationManager()
+        }
+        
+        func setupLocationManager(){
+            locationManager = CLLocationManager()
+            guard let locationManager = locationManager else { return }
+            
+            locationManager.requestAlwaysAuthorization()
+            
+            let status = CLLocationManager.authorizationStatus()
+            if status == .authorizedAlways {
+                locationManager.distanceFilter = 10
+                locationManager.startUpdatingLocation()
+            }
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let location = locations.first
+            let latitude = location?.coordinate.latitude
+            let longitude = location?.coordinate.longitude
+            
+            print("latitude: \(latitude!)\nlongitude: \(longitude!)")
+        }
+        
         func GetLocationLatitude() -> Double {
             return -150.0
         }
--- SeatMatchingiOS/SeatMatchingiOS/ViewController.swift
+++ SeatMatchingiOS/SeatMatchingiOS/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupLocationManager()
+       
+       
     }
 
     func GetActive() -> Bool{
         return false
     }
     
+    var locationManager: CLLocationManager!
+    
+    func setupLocationManager(){
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedAlways {
+            print("authoried!!")
+            locationManager.delegate = self
+            locationManager.distanceFilter = 0
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        
+        print("latitude: \(latitude!)\nlongitude: \(longitude!)")
+    }
+    
+    func GetLocationLatitude() -> Double {
+        return -150.0
+    }
 }
 
